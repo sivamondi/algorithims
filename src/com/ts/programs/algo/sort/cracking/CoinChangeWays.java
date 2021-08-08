dbe2e78851ecc41be09014fdb8257196679fc139
27programs <sivaramarao.mondi@gmail.com>
2021-08-08 15:53:16 -0400

    initial commit

--- src/com/ts/programs/algo/sort/cracking/CoinChangeWays.java
+++ src/com/ts/programs/algo/sort/cracking/CoinChangeWays.java
@@ -1,5 +1,7 @@
 package com.ts.programs.algo.sort.cracking;
 
+import java.util.Arrays;
+
 public class CoinChangeWays {
 
     /*
@@ -10,40 +12,25 @@ public class CoinChangeWays {
     public static void main(String[] args) {
 
         int[] coins = {1, 2, 5};
-        int amount = 5;
+        int amount = 1;
 
-        int[][] map = new int[amount+ 1][coins.length];
+        System.out.println(numberOfWays(coins, amount));
 
-        int ways =  numberOfWays(amount, coins);
-
-        System.out.println("ways..."+ways);
 
     }
+    public static int numberOfWays(int coins[], int amount) {
 
-    public static int numberOfWays(int amount, int[] coins) {
+        int [] dpArray = new int[amount+1];
 
-        int[][] dpArray = new int[amount + 1][1];
+        dpArray[0] = 1;
 
-        // fill array with default values, larger than amount
-        for (int i = 0; i < dpArray.length; i++) {
-            dpArray[i][0] = -1;
-        }
-
-        // minimum coins required to fill zero coins are zero
-        dpArray[0][0] = 0;
-
-
-        for (int i = 0; i < coins.length; i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (coins[i] <= j) {
-
-                    int ways = Math.max(dpArray[j][0], dpArray[j - coins[i]][0]);
-
-                    dpArray[j][0] = ways;
-                }
+        for (int i=0; i<coins.length; i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                int previousDp = dpArray[j - coins[i]];
+                dpArray[j] = dpArray[j] + previousDp;
             }
         }
 
-        return dpArray[amount][0] == Integer.MAX_VALUE ? -1 : dpArray[amount][0];
+        return dpArray[amount];
     }
 }
---
+++ src/com/ts/programs/algo/sort/cracking/FirstUniqueCharacter.java
@@ -0,0 +1,41 @@
+package com.ts.programs.algo.sort.cracking;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+
+ *  Optimizing
+ */
+public class FirstUniqueCharacter {
+    public static void main(String[] args) {
+        String a = "abac";
+        int index = uniqueCharIndex(a);
+
+        System.out.println("match="+index);
+    }
+
+    public static int uniqueCharIndex(String s) {
+        Map<Character, Integer> charFreq = new HashMap<>();
+
+        for(int i=0;i<s.length();i++) {
+            if(!charFreq.containsKey(s.charAt(i))) {
+                charFreq.put(s.charAt(i), i);
+            }else {
+                charFreq.put(s.charAt(i), s.length()+1);
+            }
+        }
+
+        int minIndex = Integer.MAX_VALUE;
+
+        for(int i: charFreq.values()) {
+            minIndex = Math.min(minIndex, i);
+        }
+
+
+        return minIndex > s.length() ? -1 : minIndex;
+
+    }
+}
