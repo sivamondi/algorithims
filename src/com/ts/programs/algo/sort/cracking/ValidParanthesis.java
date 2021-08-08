cae68a31f170b624d79b217548b109a9c31e4e56
27programs <sivaramarao.mondi@gmail.com>
2021-08-08 11:50:14 -0400

    initial commit

--- src/com/ts/programs/algo/sort/cracking/ValidParanthesis.java
+++ src/com/ts/programs/algo/sort/cracking/ValidParanthesis.java
@@ -1,20 +1,20 @@
 package com.ts.programs.algo.sort.cracking;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class ValidParanthesis {
 
     /*
-   Valid Parentheses: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-An input string is valid if:
-Open brackets must be closed by the same type of brackets.
-Open brackets must be closed in the correct order.
-
+     Valid Parentheses: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+     An input string is valid if:
+        Open brackets must be closed by the same type of brackets.
+        Open brackets must be closed in the correct order.
      */
-
     public static void main(String[] args) {
 
-        String s = "{}";
+        String s = "{{}}";
 
         boolean valid =  isValid(s);
 
@@ -25,14 +25,17 @@ Open brackets must be closed in the correct order.
     public static boolean isValid(String s) {
 
         Stack<Character> stack = new Stack<Character>();
+        Map<Character, Character > paranthesisPairs = new HashMap<>();
+        paranthesisPairs.put('(',')');
+        paranthesisPairs.put('{','}');
+        paranthesisPairs.put('[',']');
+
+
         for(char c: s.toCharArray()) {
-            if(c == '(' || c == '[' || c == '{') {
+
+            if(paranthesisPairs.containsKey(c)) {
                 stack.push(c);
-            } else if( c== ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if( c == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
-            } else if(c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+            }else if(paranthesisPairs.containsValue(c) && !stack.isEmpty() && c == paranthesisPairs.get(stack.peek()) ) {
                 stack.pop();
             } else {
                 return false;
